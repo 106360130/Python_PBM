46e6e1005a123cf22c2a75e9a731763bc0fe7961
106360130 <bs25017@gmail.com>
2021-05-16 21:56:26 +0800

    Comparing strings

---
+++ comparing_string.py
@@ -0,0 +1,17 @@
+#比較字串
+dict = {
+"status" : "OK",
+"status_2" : 
+}
+
+print(dict["status"])
+
+if dict["status"] == "OK" :
+	print("True")
+
+if dict["status"] is "OK" :
+	print("True")
+
+if dict["status_2"] == "OK" :
+	print("True_2")
+#比較字串
